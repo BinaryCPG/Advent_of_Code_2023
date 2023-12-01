605ec603e440fa21c4fb356220620a91fc526651
BinaryCPG <iabg.tn@gmail.com>
2023-12-01 17:22:21 +0100

    Day 01 - 1

---
+++ Day_01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent_of_Code_2023 | Day_01 | 1");
+            string[] input = File.ReadAllLines("input.txt");
+            int sum = 0;
+            for(int i = 0; i < input.Length; i++)
+            {
+                char[] digits = input[i].ToCharArray().Where(x => x >= 48 && x <= 57).ToArray();
+                sum += (((digits[0] - 48) * 10) + (digits[digits.Length - 1]-48));
+            }
+            Console.WriteLine($"Sum: {sum}");
+            Console.ReadLine();
+        }
+    }
+}
