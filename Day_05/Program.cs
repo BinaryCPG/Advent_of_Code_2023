47af697e9267bdb9c2cd92440c0ddddd8a088116
BinaryCPG <iabg.tn@gmail.com>
2023-12-05 10:27:55 +0100

    Advent_of_Code_2023 | Day_05 | 2 - Inprog

--- Day_05/Program.cs
+++ Day_05/Program.cs
@@ -89,7 +89,77 @@ namespace Day_05
             Console.WriteLine("Advent_of_Code_2023 | Day_05 | 2");
             sum = 0;
 
+            List<Tuple<Int64, Int64>> seedRanges = new List<Tuple<long, long>>();
+            {
+                string[] seedLine = input[0].Split(new char[] { ':' })[1].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                for(int i = 0; i < seedLine.Length; i += 2)
+                {
+                    seedRanges.Add(new Tuple<Int64, Int64>(Int64.Parse(seedLine[i]), Int64.Parse(seedLine[i+1])));
+                }
+            }
+            seedRanges.Sort((a, b) => a.Item1.CompareTo(b.Item1));
+            /*
+            A-to-B map:
+                [offset value]
+                    [
+                        [lower range],[length]
+                    ]
+             */
+            List<Dictionary<Int64, List<Tuple<Int64, Int64>>>> offsetMatrix = new List<Dictionary<long, List<Tuple<long, long>>>>();
+            Dictionary<Int64, List<Tuple<Int64, Int64>>> newOffsetBlock = null;
+            for (Int64 i = 1; i < input.Length; i++)
+            {
+                if (input[i].Length > 0)
+                {
+                    if (48 <= input[i][0] && input[i][0] <= 57)
+                    {
+                        if(newOffsetBlock == null)
+                        {
+                            newOffsetBlock = new Dictionary<long, List<Tuple<long, long>>>();
+                        }
 
+                        string[] line = input[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        if (line.Length != 3)
+                            throw new ArgumentOutOfRangeException($"Expected line.Length==3 but line.Length=={line.Length} ({line})");
+                        Int64 destOffset = Int64.Parse(line[0].Trim());
+                        Int64 srcOffset = Int64.Parse(line[1].Trim());
+                        Int64 rngLength = Int64.Parse(line[2].Trim());
+                        Int64 offset = destOffset - srcOffset;
+
+                        if (!newOffsetBlock.ContainsKey(offset))
+                        {
+                            newOffsetBlock.Add(offset, new List<Tuple<long, long>>());
+                        }
+                        newOffsetBlock[offset].Add(new Tuple<long, long>(srcOffset, rngLength));
+                    }
+                }
+                else
+                {
+                    if (newOffsetBlock != null)
+                    {
+                        offsetMatrix.Add(newOffsetBlock);
+                        newOffsetBlock = null;
+                    }
+                }
+            }
+            if (newOffsetBlock != null)
+            {
+                offsetMatrix.Add(newOffsetBlock);
+                newOffsetBlock = null;
+            }
+
+            List<List<Int64>> offsetsSorted = new List<List<long>>();
+            foreach(var offsetBlock in offsetMatrix)
+            {
+                List<Int64> offsets = new List<Int64>(offsetBlock.Keys);
+                offsets.Sort((a, b) => a.CompareTo(b));
+                offsetsSorted.Add(offsets);
+            }
+
+            while (sum == 0)
+            {
+
+            }
 
             Console.WriteLine($"Sum: {sum}");
 
